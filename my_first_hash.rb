8239679040b07442e02cf9fa16c11b44e6f08873
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 18:22:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,43 +4,43 @@ my_hash = {"name" => "Kevin"}
 
 end
 
-# 
-# def shipping_manifest
-#
-#   the_manifest = {
-#     "whale bone corsets" => 5,
-#     "porcelain vases" => 2,
-#     "oil paintings" = 3
-#   }
-# end
-#
-# def retrieval
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5,
-#                   "porcelain vases" => 2,
-#                   "oil paintings" => 3
-#                  }
-#
-#                  hipping_manifest["oil paintings"]
-#
-#
-# end
-#
-# def adding
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5,
-#                   "porcelain vases" => 2,
-#                   "oil paintings" => 3
-#                  }
-#   # add 2 muskets to the shipping_manifest hash below
-# shipping_manifest["muskets"] = 2
-#
-#
-#   # add 4 gun powder to the shipping_manifest hash below
-# shipping_manifest["gun powder"] = 4
-#
-#
-#   # return the shipping_manifest hash below
-#   return shipping_manifest
-#
-# end
+
+def shipping_manifest
+
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" = 3
+  }
+end
+
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+
+                 hipping_manifest["oil paintings"]
+
+
+end
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+  # add 2 muskets to the shipping_manifest hash below
+shipping_manifest["muskets"] = 2
+
+
+  # add 4 gun powder to the shipping_manifest hash below
+shipping_manifest["gun powder"] = 4
+
+
+  # return the shipping_manifest hash below
+  return shipping_manifest
+
+end
