6042b6d2617bbd628bd1a1f1e49c025ada333cf2
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 18:28:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,33 +14,33 @@ def shipping_manifest
   }
 end
 #
-# def retrieval
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5,
-#                   "porcelain vases" => 2,
-#                   "oil paintings" => 3
-#                  }
-#
-#                  hipping_manifest["oil paintings"]
-#
-#
-# end
-#
-# def adding
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5,
-#                   "porcelain vases" => 2,
-#                   "oil paintings" => 3
-#                  }
-#   # add 2 muskets to the shipping_manifest hash below
-# shipping_manifest["muskets"] = 2
-#
-#
-#   # add 4 gun powder to the shipping_manifest hash below
-# shipping_manifest["gun powder"] = 4
-#
-#
-#   # return the shipping_manifest hash below
-#   return shipping_manifest
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
 
-# end
+                 shipping_manifest["oil paintings"]
+
+
+end
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+  # add 2 muskets to the shipping_manifest hash below
+shipping_manifest["muskets"] = 2
+
+
+  # add 4 gun powder to the shipping_manifest hash below
+shipping_manifest["gun powder"] = 4
+
+
+  # return the shipping_manifest hash below
+  return shipping_manifest
+
+end
