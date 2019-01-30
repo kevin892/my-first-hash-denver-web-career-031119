237314d0e9c2a276d513d7cf8e80f196078a4297
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 18:22:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,46 +1,46 @@
-# def my_hash
-#   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-# hash = {"name" => "Kevin"}
+def my_hash
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+my_hash = {"name" => "Kevin"}
+
+end
+
+# 
+# def shipping_manifest
+#
+#   the_manifest = {
+#     "whale bone corsets" => 5,
+#     "porcelain vases" => 2,
+#     "oil paintings" = 3
+#   }
+# end
+#
+# def retrieval
+#   shipping_manifest = {
+#                   "whale bone corsets" => 5,
+#                   "porcelain vases" => 2,
+#                   "oil paintings" => 3
+#                  }
+#
+#                  hipping_manifest["oil paintings"]
+#
 #
 # end
-
-
-def shipping_manifest
-
-  the_manifest = {
-    "whale bone corsets" => 5,
-    "porcelain vases" => 2,
-    "oil paintings" = 3
-  }
-end
-
-def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-
-                 hipping_manifest["oil paintings"]
-
-
-end
-
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"] = 2
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest["gun powder"] = 4
-
-
-  # return the shipping_manifest hash below
-  return shipping_manifest
-
-end
+#
+# def adding
+#   shipping_manifest = {
+#                   "whale bone corsets" => 5,
+#                   "porcelain vases" => 2,
+#                   "oil paintings" => 3
+#                  }
+#   # add 2 muskets to the shipping_manifest hash below
+# shipping_manifest["muskets"] = 2
+#
+#
+#   # add 4 gun powder to the shipping_manifest hash below
+# shipping_manifest["gun powder"] = 4
+#
+#
+#   # return the shipping_manifest hash below
+#   return shipping_manifest
+#
+# end
