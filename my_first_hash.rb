590e67bfba5db0ad2e0b07c7d05e9fe7a0929dd3
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 18:27:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,14 +5,14 @@ my_hash = {"name" => "Kevin"}
 end
 
 
-# def shipping_manifest
-#
-#   shipping_manifest = {
-#     "whale bone corsets" => 5,
-#     "porcelain vases" => 2,
-#     "oil paintings" = 3
-#   }
-# end
+def shipping_manifest
+
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" = 3
+  }
+end
 #
 # def retrieval
 #   shipping_manifest = {
