174d6d7d238d6fecbbd805d894c38392b667f0ac
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 18:11:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,12 +37,12 @@ end
 #                  }
 #   # add 2 muskets to the shipping_manifest hash below
 # shipping_manifest["muskets"] = 2
-# 
-# 
+#
+#
 #   # add 4 gun powder to the shipping_manifest hash below
 # shipping_manifest["gun powder"] = 4
-# 
-# 
+#
+#
 #   # return the shipping_manifest hash below
-# 
+#
 # end
