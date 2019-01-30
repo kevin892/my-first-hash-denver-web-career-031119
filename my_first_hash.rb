4b5311042c6ee4633e25762758e5e189f6ac2908
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 18:04:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,17 +10,17 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   the_manifest = {
-    "whale bone corsets" => 5,
-    "porcelain vases" => 2,
-    "oil paintings" = 3
+    "whale bone corsets" => "5",
+    "porcelain vases" => "2",
+    "oil paintings" = "3"
   }
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "whale bone corsets" => "5",
+                  "porcelain vases" => "2",
+                  "oil paintings" => "3"
                  }
 
                  hipping_manifest["oil paintings"]
@@ -31,16 +31,16 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "whale bone corsets" => "5",
+                  "porcelain vases" => "2",
+                  "oil paintings" => "3"
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"] = 2
+shipping_manifest["muskets"] = "2"
 
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest["gun powder"] = 4
+shipping_manifest["gun powder"] = "4"
 
 
   # return the shipping_manifest hash below
